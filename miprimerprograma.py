88f6a963dc819785bf368e4b30d67c9691c8ed22
Luis Pinos <149178337+LAPinosB@users.noreply.github.com>
2024-03-10 13:31:28 +0100

    version 1.0
    Empezando

---
+++ miprimerprograma.py
@@ -0,0 +1,6 @@
+import numpy as np #hay que tener instalado esta libreria desde python sino no funciona como me estabapasando a mi
+
+msg = "Roll a dice"
+print(msg)
+
+print(np.random.randint(1,9))
